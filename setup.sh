b4b53cfca30eb7e7ef621856fea31bac1921590a
Marius <80470007+mkah91@users.noreply.github.com>
2022-03-09 15:07:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install unclutter
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+sudo apt-get install --no-install-recommends chromium-browser
