4e50718643b14991c8edccd349ce402c58763e2b
Marius <80470007+mkah91@users.noreply.github.com>
2022-03-09 15:27:42 +0100

    Update start-divera-monitor.sh

--- start-divera-monitor.sh
+++ start-divera-monitor.sh
@@ -6,6 +6,7 @@
 xset -dpms # turn off display power management system
 xset s noblank # turn off screen blanking
 xset s off # turn off screen saver
+
 # Remove exit errors from the config files that could trigger a warning
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
