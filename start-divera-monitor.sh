0d75bb263ea53af78c1b59b309ce6a38017b6e9c
Marius <80470007+mkah91@users.noreply.github.com>
2022-03-09 15:26:39 +0100

    Update start-divera-monitor.sh

--- start-divera-monitor.sh
+++ start-divera-monitor.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # needed environment variables: MONITOR_URL
-chromium-browser --noerrdialogs --kiosk --incognito $MONITOR_URL &>/dev/null
+
+# Display settings
+xset -dpms            # turn off display power management system
+xset s noblank        # turn off screen blanking
+xset s off            # turn off screen saver
+# Remove exit errors from the config files that could trigger a warning
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromi$
+
+# Run Chromium in kiosk mode
+chromium-browser  --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --incognito --kiosk $MONITOR_URL
